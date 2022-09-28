927edd9a198ce6cb04daaa6ad76e55eb912ef5c0
wildNoodleBrain <104745283+wildNoodleBrain@users.noreply.github.com>
2022-09-27 22:00:01 -0500

    csharp web dev exceptions exercise

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,21 +7,52 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+        
         static double Divide(double x, double y)
         {
             // Write your code here!
+            if (y == 0.0)
+            {
+                throw new ArgumentOutOfRangeException("y", "You can't divide by zero!");
+            }
+            else
+            {
+                return x / y;   
+            }
         }
 
         static int CheckFileExtension(string fileName)
         {
             // Write your code here!
-        }
-        */
+            if (fileName == null || fileName == "")
+            {
+                throw new ArgumentNullException("fileName","Student did not submit any work.");
+            }
+            else
+            {
+                if (fileName.Substring(fileName.Length - 3, 3) == ".cs") //??
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }        
 
         static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            try
+            {
+                Divide(num1,  num2);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
